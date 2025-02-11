c92e7f69c1a588e9d15206bf9274666f16946bc3
JoaoVCCaetano <joao.vit.caetano@gmail.com>
2025-02-11 18:04:15 -0300

    alteracoes de ambiente

--- src/routes.php
+++ src/routes.php
@@ -7,11 +7,8 @@ $indexController = new IndexController();
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($uri) {
-    case '/index':
+    default:
         $indexController->index();
         break;
-    default:
-        http_response_code(404);
-        echo 'Page not found';
-        break;
+        
 }
