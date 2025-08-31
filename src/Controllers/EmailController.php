cdf06475d66b2ad6e48b5a6cfee9542eb82b9a65
Bernardo <bernardo.niehues@mutualizo.com>
2025-08-30 21:28:44 -0300

    Att

--- src/Controllers/EmailController.php
+++ src/Controllers/EmailController.php
@@ -12,17 +12,6 @@ class EmailController {
 
         try {
 
-            $sesClient = new SesClient([
-                'version' => '2010-12-01',
-                'region'  => 'us-east-2',
-                'region'  => getenv('AWS_REGION'),
-                'credentials' => [
-                    'key'    => '',
-                    'secret' => '',
-                    'key'    => getenv('AWS_ACCESS_KEY_ID'),
-                    'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
-                ],
-            ]);
 
             $senderEmail = 'bernardoniehues01@gmail.com';
             $subject = 'Teste de E-mail AWS SES';
